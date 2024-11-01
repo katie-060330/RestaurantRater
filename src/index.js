83ed75894e5a9b165774f38697c1329735fe32bb
katie-060330 <kate.lonsway@outlook.com>
2024-11-01 12:45:06 -0400

    adding the register of users

--- src/index.js
+++ src/index.js
@@ -15,11 +15,46 @@ const firebaseConfig = {
     appId: "1:934572403483:web:f8082552386690f01381be"
   };
 
-  //!initlaize the app
+  //*initlaize the app
   initializeApp(firebaseConfig)
 
-  //! initliazing the databse 
+  //* initliazing the databse 
   const db = getFirestore(); 
+
+  //*collection refrence 
+  const colRef = collection(db, 'users')
+
+  //? Registering users to the databse 
+
+  const registerFrom = document.querySelector('.register')
+  registerFrom.addEventListener('submit', (e) =>{
+    e.preventDefault(); 
+    //TODO check to see if the user is already rediterd if so do not allow the register of the user
+
+    addDoc(colRef,{
+      firstName: registerFrom.firstName.value,
+      lastName: registerFrom.lastName.value, 
+      email: registerFrom.email.value, 
+      password: registerFrom.password.value
+    } )
+    .then(()=>{
+      //TODO send the user to the home page 
+      registerFrom.reset();
+    })
+  })
+
+  // const addBookForm = document.querySelector('.add')
+// addBookForm.addEventListener('submit', (e) => {
+//   e.preventDefault()
+
+//   addDoc(colRef, {
+//     title: addBookForm.title.value,
+//     author: addBookForm.author.value,
+//   })
+//   .then(() => {
+//     addBookForm.reset()
+//   })
+// })
 
 
 
@@ -37,25 +72,25 @@ const firebaseConfig = {
 
 //!getting data
   //?collection refrence 
-  const colRef = collection(db, 'box')
+
 
   //quereys 
   //?get the field that are equal to rober munch 
-  const q = query(colRef, where("title", "==", "robert munch"))
+  // const q = query(colRef, where("title", "==", "robert munch"))
 
   //?get the colection data
 
   
 //TODO to get the users equal to something use q defined above 
   //!Real time listners
-  onSnapshot(colRef, (snapshot)=>{
-    let box = []
-    snapshot.docs.forEach((doc) =>{
-        box.push({... doc.data(), id: doc.id})
-    })
-    console.log(box);
+  // onSnapshot(colRef, (snapshot)=>{
+  //   let box = []
+  //   snapshot.docs.forEach((doc) =>{
+  //       box.push({... doc.data(), id: doc.id})
+  //   })
+  //   console.log(box);
 
-  })
+  // })
 
     
     
@@ -63,32 +98,32 @@ const firebaseConfig = {
   //!adding 
 
 
-const addUserForm = document.querySelector('.loginForm')
-addUserForm.addEventListener('submit', (e) =>{
-    e.preventDefault(); 
-    addDoc(colRef, {
-        author: addUserForm.email.value, 
-        title: addUserForm.password.value
-    })
-    .then(()=>{
-        addUserForm.reset();
-    })
-})
+// const addUserForm = document.querySelector('.loginForm')
+// addUserForm.addEventListener('submit', (e) =>{
+//     e.preventDefault(); 
+//     addDoc(colRef, {
+//         author: addUserForm.email.value, 
+//         title: addUserForm.password.value
+//     })
+//     .then(()=>{
+//         addUserForm.reset();
+//     })
+// })
 
 
 
 //!deleting data
 
-const deleteUserForm = document.querySelector('.delete')
-deleteUserForm.addEventListener('submit', (e)=>{
-    e.preventDefault(); 
+// const deleteUserForm = document.querySelector('.delete')
+// deleteUserForm.addEventListener('click', (e)=>{
+//     e.preventDefault(); 
 
-    const docRef = doc(db, 'box', deleteUserForm.id.value)
-    deleteDoc(docRef)
-    .then(() =>{
-        deleteUserForm.reset();
-    })
-})
+//     const docRef = doc(db, 'box', deleteUserForm.id.value)
+//     deleteDoc(docRef)
+//     .then(() =>{
+//         deleteUserForm.reset();
+//     })
+// })
 
 // const deleteBookForm = document.querySelector('.delete')
 // deleteBookForm.addEventListener('submit', (e) => {
